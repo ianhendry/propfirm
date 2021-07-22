9de8957d3b815cf4bf592926bce7c3d33e36125d
ianhendry <ianhendry@yahoo.com>
2021-07-22 16:22:45 +0800

    pull challenge and trade data in to the dashboard

--- src/main/webapp/app/account/dashboard/dashboard.component.ts
+++ src/main/webapp/app/account/dashboard/dashboard.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 
 import { ISiteAccount } from '../../entities/site-account/site-account.model';
+import { IMt4Trade } from '../../entities/mt-4-trade/mt-4-trade.model';
+import { IMt4Account } from '../../entities/mt-4-account/mt-4-account.model';
 import { ITradeChallenge } from '../../entities/trade-challenge/trade-challenge.model';
 import { IUser } from '../../entities/user/user.model';
 import { SiteAccountService } from '../../entities/site-account/service/site-account.service';
 import { TradeChallengeService } from '../../entities/trade-challenge/service/trade-challenge.service';
+import { Mt4TradeService } from '../../entities/mt-4-trade/service/mt-4-trade.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { UserManagementService } from 'app/admin/user-management/service/user-management.service';
 import { DataUtils } from 'app/core/util/data-util.service';
@@ -16,23 +19,26 @@ import { DataUtils } from 'app/core/util/data-util.service';
 })
 export class DashboardComponent implements OnInit {
 	siteAccounts?: ISiteAccount[] | null = null;
-  	tradeChallenges: ITradeChallenge[] = [];
-	tradeChallenge?: ITradeChallenge[] | null = null;
+  	mt4Trades?: IMt4Trade[] | null = null;
+	tradeChallenges: ITradeChallenge[] = [];
+	tradeChallenge?: ITradeChallenge;
+
   	isLoading = false;
 	user: IUser | null = null;
 	
-	data: any;
+	// data: any;
 	
 	constructor(
 		protected dataUtils: DataUtils, 
 		protected siteAccountService: SiteAccountService,
 		protected tradeChallengeService: TradeChallengeService,
+		protected mt4TradeService: Mt4TradeService,
 		private accountService: AccountService,
     	private userService: UserManagementService
-	) {}
+	) {
+	}
 
-
-	 ngOnInit(): void {
+	ngOnInit(): void {
 		this.accountService.getAuthenticationState().subscribe(
 		account => {
 		   if (account){
@@ -73,22 +79,59 @@ export class DashboardComponent implements OnInit {
 	          this.isLoading = false;
 	          this.onError();
 	        }
-	      );
-	  }
+	     );
+	}
 
 	getTradeChallenges(data: ISiteAccount[]): void {
 		for (const item of data) {
-			if (item.id) {
-				this.tradeChallengeService.find(item.id).subscribe(
-					(res: HttpResponse<ITradeChallenge>) => {
-						if (res.body) {
-							this.tradeChallenges.push(res.body)
-						}
-                    } 
-		    	);
+			if (item.tradeChallenges) {
+				for (const challenge of item.tradeChallenges) {
+					if (challenge.id) {
+						this.tradeChallengeService.find(challenge.id).subscribe(
+							(res: HttpResponse<ITradeChallenge>) => {
+								if (res.body) {
+									this.tradeChallenges.push(res.body)
+								}
+		                    } 
+				    	);
+					}
+				}
 			}
 		}
 		this.isLoading = false;
+	}
+	
+	getMt4Trades(mt4AccountId: IMt4Account):void {
+		const criteria: { key: string; value: any; }[] = [];
+	  	criteria.push({key: 'mt4AccountId.equals', value: mt4AccountId.id});
+
+		if (mt4AccountId.id) {
+			this.mt4TradeService.query({
+		        page: 0,
+		        size: 20,
+		        sort: [],
+	        	criteria
+		      }).subscribe(
+				(res: HttpResponse<IMt4Trade[]>) => {
+					if (res.body) {
+						this.mt4Trades = res.body
+					}
+	            } 
+	    	);
+		}
+	}
+	
+	
+	showChallenge(challengeId: ITradeChallenge):void {
+		if (challengeId.id) {
+			this.tradeChallengeService.find(challengeId.id).subscribe(
+				(res: HttpResponse<ITradeChallenge>) => {
+					if (res.body) {
+						this.tradeChallenge = res.body
+					}
+	            } 
+	    	);
+		}
 	}
 	
 	onError(): void {
