a0b20052c7341751591c36a05a758f672cc4836f
ianhendry <ianhendry@yahoo.com>
2021-08-10 09:13:10 +0800

    added process filter to JSON

--- src/main/java/com/gracefl/propfirm/mt4/connection/Mt4ClientConnectionAccountDataSubscriber.java
+++ src/main/java/com/gracefl/propfirm/mt4/connection/Mt4ClientConnectionAccountDataSubscriber.java
@@ -15,6 +15,9 @@ import com.gracefl.propfirm.domain.TradeChallenge;
 import com.gracefl.propfirm.domain.enumeration.BROKER;
 import com.gracefl.propfirm.domain.enumeration.TRADEDIRECTION;
 import com.gracefl.propfirm.helpers.DatesHelper;
+import com.gracefl.propfirm.mt4.connection.dto.Mt4ClientTerminalTradeObject;
+import com.gracefl.propfirm.mt4.connection.dto.Mt4LiveAccountJSON;
+import com.gracefl.propfirm.mt4.connection.dto.Mt4TradesObject;
 import com.gracefl.propfirm.repository.InstrumentRepository;
 import com.gracefl.propfirm.repository.Mt4AccountRepository;
 import com.gracefl.propfirm.repository.Mt4TradeRepository;
@@ -135,9 +138,14 @@ public class Mt4ClientConnectionAccountDataSubscriber implements Runnable, Close
 	                log.debug("account info recievd {}", myJSONstring);
 	                
 	                ObjectMapper objectMapper = new ObjectMapper();
-
-	                Mt4LiveAccountJSON accountInformation = objectMapper.readValue(myJSONstring, Mt4LiveAccountJSON.class);
-	                processAccountDataTimeSeries(accountInformation, mt4Account);
+	                
+	                if (myJSONstring.contains("ACCOUNT_INFORMATION")) {
+	                	Mt4LiveAccountJSON accountInformation = objectMapper.readValue(myJSONstring, Mt4LiveAccountJSON.class);
+	                	processAccountDataTimeSeries(accountInformation, mt4Account);
+	                } else {
+	                	Mt4ClientTerminalTradeObject accountInformation = objectMapper.readValue(myJSONstring, Mt4ClientTerminalTradeObject.class);
+	                	processJSONMt4TRades(accountInformation, mt4Account);
+	                }
 	            }
 	        }
 	        catch(org.zeromq.ZMQException | JsonProcessingException ex){
@@ -168,32 +176,33 @@ public class Mt4ClientConnectionAccountDataSubscriber implements Runnable, Close
     		Double equity = processObject.getAccountInformation().getAccountEquity();
     		Double balance = processObject.getAccountInformation().getAccountBalance();
     		Double openProfit = processObject.getAccountInformation().getAccountProfit();
-    		Double currentDrawdown = openProfit;
+    		Double currentDrawdown;
+    		if (openProfit < 0) {
+    			currentDrawdown = openProfit;
+    		} else {
+    			currentDrawdown = 0D;
+    		}
     		
-			AccountDataTimeSeries accountDataTimeSeries = new AccountDataTimeSeries();
-	        accountDataTimeSeries.setAccountBalance(balance);
-	        accountDataTimeSeries.setAccountEquity(equity);
-	        accountDataTimeSeries.setAccountCredit(processObject.getAccountInformation().getAccountCredit());
-	        accountDataTimeSeries.setAccountFreeMargin(processObject.getAccountInformation().getAccountFreeMargin());
-	        accountDataTimeSeries.setAccountStopoutLevel(processObject.getAccountInformation().getAccountStopoutLevel().intValue());
-	        accountDataTimeSeries.setOpenLots(processObject.getAccountInformation().getOpenLots());
-	        accountDataTimeSeries.setOpenNumberOfTrades(processObject.getAccountInformation().getOrdersTotal().intValue());
-	        accountDataTimeSeries.setMt4Account(mt4Account);
-	        accountDataTimeSeries.setDateStamp(timeStamp);
-	        
 	        TradeChallenge challenge = mt4Account.getTradeChallenge();	        
-	        Double lastDrawdown = challenge.getRunningMaxTotalDrawdown();
+	        Double runningMaxDrawdown = challenge.getRunningMaxTotalDrawdown();
+	        Double runningDailyDrawdown = challenge.getRunningMaxDailyDrawdown();
+	        Double lastDrawdown = mt4Account.getAccountFreeMarginMode();
 	        
-	        Double percentChange = currentDrawdown * 100 / openProfit;
+	        Double percentChange;
 	        
+	        if (openProfit > 0 || openProfit < 0) {
+	        	percentChange =  lastDrawdown * 100 / currentDrawdown;
+	        } else {
+	        	percentChange = 0D;
+	        }
 	        // need to think about how to calculate this daily drawdown 
 	        Double dailyDrawdown = mt4Account.getAccountInfoDouble() - currentDrawdown;
 	        
-	        if (currentDrawdown < challenge.getRunningMaxTotalDrawdown()) {
+	        if (currentDrawdown < runningMaxDrawdown) {
 	        	challenge.setRunningMaxTotalDrawdown(currentDrawdown);
 	        }
-	        if (currentDrawdown < challenge.getRunningMaxDailyDrawdown()) {
-	        	challenge.setRunningMaxDailyDrawdown(balance);
+	        if (currentDrawdown < runningDailyDrawdown) {
+	        	challenge.setRunningMaxDailyDrawdown(currentDrawdown);
 	        }
 	        
 	        // total drawdown check
@@ -215,13 +224,30 @@ public class Mt4ClientConnectionAccountDataSubscriber implements Runnable, Close
 	        
 	        // only save if a) rules broken or b) equity has changed by more than 0.01% from last time to limit number records needly saved
 	        if (percentChange > 0.01 || rulesBroken) {
+	        	
+	        	AccountDataTimeSeries accountDataTimeSeries = new AccountDataTimeSeries();
+		        accountDataTimeSeries.setAccountBalance(balance);
+		        accountDataTimeSeries.setAccountEquity(equity);
+		        accountDataTimeSeries.setAccountCredit(processObject.getAccountInformation().getAccountCredit());
+		        accountDataTimeSeries.setAccountFreeMargin(processObject.getAccountInformation().getAccountFreeMargin());
+		        accountDataTimeSeries.setAccountStopoutLevel(processObject.getAccountInformation().getAccountStopoutLevel().intValue());
+		        accountDataTimeSeries.setOpenLots(processObject.getAccountInformation().getOpenLots());
+		        accountDataTimeSeries.setOpenNumberOfTrades(processObject.getAccountInformation().getOrdersTotal().intValue());
+		        accountDataTimeSeries.setMt4Account(mt4Account);
+		        accountDataTimeSeries.setDateStamp(timeStamp);
+		        
 		        accountDataTimeSeriesService.save(accountDataTimeSeries);
 		        
 		        mt4Account.setAccountEquity(equity);
 		        mt4Account.setAccountBalance(balance);
-		        mt4AccountRepository.save(mt4Account);
+		        
+		       
     		}
     	
+	        // used to calculate the % change in draw down each run
+	        mt4Account.setAccountFreeMarginMode(currentDrawdown);
+	        mt4AccountRepository.save(mt4Account);
+	        
 	        tradeChallengeRepository.save(challenge);
 	        
     	}
@@ -244,4 +270,89 @@ public class Mt4ClientConnectionAccountDataSubscriber implements Runnable, Close
 		}
 		*/
     }
+    
+ private void processJSONMt4TRades(Mt4ClientTerminalTradeObject processObject, Mt4Account mt4Account) {
+		
+    	Double balance = mt4Account.getAccountBalance();
+    	Double equity = mt4Account.getAccountEquity();
+    	
+    	for (Mt4TradesObject trade: processObject.getTrades()) {
+    		
+    		if (trade.getSymbol().length()>1) {
+    			
+    			//TODO check here if the trade is already stored and do not duplicate 
+    			if (!checkMt4TradeExists(trade.getSymbol(), trade.getTicket())) {
+	    			TRADEDIRECTION tradeDirection; 
+	    			if (trade.getType() == 1) {
+	    				tradeDirection = TRADEDIRECTION.SELL; 
+	    			} else {
+	    				tradeDirection = TRADEDIRECTION.BUY;
+	    			}
+	    				
+	    			Instrument instrument = getIntrumentFromTicker(trade.getSymbol()); 			
+	    			
+	    			Mt4Trade mt4Trade = new Mt4Trade();
+	    			mt4Trade.setClosePrice(trade.getOpenPrice().doubleValue());
+	    			mt4Trade.setCloseTime(datesHelper.convertStringToInstant(trade.getOpenTime()));
+	    			mt4Trade.setCommission(trade.getCommission().doubleValue());
+	    			mt4Trade.setDirectionType(tradeDirection);
+	    			mt4Trade.setInstrument(instrument);
+	    			mt4Trade.setMt4Account(mt4Account);
+	    			mt4Trade.setOpenPrice(trade.getOpenPrice().doubleValue());
+	    			mt4Trade.setPositionSize(trade.getLots());
+	    			mt4Trade.setStopLossPrice(trade.getStopLoss().doubleValue());
+	    			mt4Trade.setSymbol(trade.getSymbol());
+	    			mt4Trade.setTakeProfitPrice(trade.getTakeProfit().doubleValue());
+	    			mt4Trade.setTicket(BigDecimal.valueOf(trade.getTicket()));
+	    			mt4Trade.setSwap(trade.getSwap().doubleValue());
+	    			mt4Trade.setProfit(trade.getProfit().doubleValue());
+	    			
+	    			mt4TradeRepository.save(mt4Trade);
+	    			
+	    			balance = balance + trade.getProfit().doubleValue();
+	    			equity = equity + trade.getProfit().doubleValue();
+	    			AccountDataTimeSeries accountDataTimeSeries = new AccountDataTimeSeries();
+	    	        accountDataTimeSeries.setAccountBalance(balance);
+	    	        accountDataTimeSeries.setAccountEquity(equity);
+	    	        accountDataTimeSeries.setMt4Account(mt4Account);
+	    	        accountDataTimeSeries.setDateStamp(datesHelper.convertStringToInstant(trade.getOpenTime()));
+	    	        accountDataTimeSeriesService.save(accountDataTimeSeries);
+	    	        
+	    	        mt4Account.addAccountDataTimeSeries(accountDataTimeSeries);
+	    	        mt4AccountRepository.save(mt4Account);
+    			} else {
+    				log.debug("Trade is already in the database. Did not update {} ", trade);
+    			}
+    		}
+    	}
+	}
+    
+    private Instrument getIntrumentFromTicker(String ticker) {
+    	Instrument instrument = new Instrument();
+		instrument.setTicker(ticker);
+		Optional<Instrument> inst = instrumentRepository.findOne(Example.of(instrument));
+		
+		if (inst.isPresent()) {
+			return inst.get();
+		} else {
+			return null;
+		}
+		
+    }
+    
+	private Boolean checkMt4TradeExists(String symbol, Double ticket ) {
+		
+		Mt4Trade tradeExample = new Mt4Trade();
+		tradeExample.setSymbol(symbol);
+		tradeExample.setTicket(BigDecimal.valueOf(ticket));
+	
+		Optional<Mt4Trade> mt4Trade = mt4TradeRepository.findOne(Example.of(tradeExample));
+		
+		if (mt4Trade.isPresent()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
--- src/main/java/com/gracefl/propfirm/mt4/connection/Mt4ClientConnectionStartUpManager.java
+++ src/main/java/com/gracefl/propfirm/mt4/connection/Mt4ClientConnectionStartUpManager.java
@@ -12,8 +12,8 @@ public class Mt4ClientConnectionStartUpManager {
 
 	private final Logger log = LoggerFactory.getLogger(Mt4ClientConnectionStartUpManager.class);
 	
-	@Autowired
-	Mt4ClientConnectionTradeHistoryPuller mt4ClientConnectionTradeHistoryPuller;
+	//@Autowired
+	//Mt4ClientConnectionTradeHistorySynchronizer mt4ClientConnectionTradeHistorySynchronizer;
 	
 	@Autowired
 	Mt4ClientConnectionAccountDataSubscriber mt4ClientConnectionAccountDataSubscriber;
@@ -21,15 +21,12 @@ public class Mt4ClientConnectionStartUpManager {
 	@PostConstruct
     public void init() throws Exception {	
 						
-		Boolean pullHistory = false;
-		
-		// get the account history
-		if (pullHistory) {
-	    	log.info("Call MT4 Client Application for MT4 trade history");
-	        Thread pullThread = new Thread(mt4ClientConnectionTradeHistoryPuller);
-	        pullThread.start();
-		}
-		
+		// MT4 trade synchronizer
+		/*
+    	log.info("Call MT4 Client Application for MT4 trade history");
+        Thread pullThread = new Thread(mt4ClientConnectionTradeHistorySynchronizer);
+        pullThread.start();
+		*/
 		// open the real time account data feed from MT4
 		log.info("Call MT4 Client Application for MT4 trade history");
         Thread subscriberThread = new Thread(mt4ClientConnectionAccountDataSubscriber);
